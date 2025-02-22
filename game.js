7a714d6dc45147e7adaf9671d3a4e854387e0809
elani-web <akachukwumarvellous0@gmail.com>
2025-02-22 07:53:20 -0800

    Add files via upload

--- game.js
+++ game.js
@@ -34,10 +34,12 @@ function checkAnswer(currentLevel){
          console.log("wrong");
          playSound("wrong")
          $("body").addClass("game-over");
-      setTimeout(function () {
+      
+         setTimeout(function () {
         $("body").removeClass("game-over");
       }, 200);
       $("#level-title").text("Game Over, Press Any Key to Restart");
+      
       startOver();
       }
 }
